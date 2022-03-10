e7abe3715d340df47b30238dd4430cdfcd315219
VictorHugoMA <victor.alves1@ufu.br>
2022-03-10 10:19:25 -0300

    att tabelas de criacao sql

--- sql/sql-tabelas.sql
+++ sql/sql-tabelas.sql
@@ -1,67 +1,59 @@
-CREATE TABLE Agenda
+CREATE TABLE agenda_Trab
 (
    codigo int PRIMARY KEY auto_increment,
    data date,
    horario date,
    nome varchar(50),
    sexo varchar(50),
-   email varchar(50) UNIQUE,
-   FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON DELETE CASCADE
+   email varchar(50),
+   codigo_medico int,
+   FOREIGN KEY (codigo_medico) REFERENCES medico_Trab(codigo) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
-CREATE TABLE Pessoa
+CREATE TABLE pessoa_Trab
+(
+   codigo int PRIMARY KEY auto_increment,
+   nome varchar(50),
+   sexo varchar(50),
+   email varchar(50) UNIQUE,
+   telefone varchar(50),
+   cep char(10),
+   logradouro varchar(100),
+   cidade varchar(50),
+   estado varchar(10)
+) ENGINE=InnoDB;
+
+CREATE TABLE paciente_Trab
+(
+   codigo int PRIMARY KEY,
+   peso float,
+   altura int,
+   tipoSanguineo varchar(10),
+   FOREIGN KEY (codigo) REFERENCES pessoa_Trab(codigo) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE funcionario_Trab
+(
+   codigo int PRIMARY KEY,
+   dataContrato date,
+   salario float,
+   senhaHash varchar(255),
+   FOREIGN KEY (codigo) REFERENCES pessoa_Trab(codigo) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE medico_Trab
+(
+   codigo int PRIMARY KEY,
+   especialidade char(50),
+   crm varchar(50),
+   FOREIGN KEY (codigo) REFERENCES pessoa_Trab(codigo) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE enderecoAjax_Trab
 (
    codigo int PRIMARY KEY auto_increment,
    cep char(10),
-   endereco varchar(100),
-   bairro varchar(50),
+   logradouro varchar(100),
    cidade varchar(50),
-   id_cliente int not null,
-   FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON DELETE CASCADE
-) ENGINE=InnoDB;
-
-CREATE TABLE Paciente
-(
-   id int PRIMARY KEY auto_increment,
-   nome varchar(50),
-   cpf char(14) UNIQUE,
-   email varchar(50) UNIQUE,
-   hash_senha varchar(255),
-   data_nascimento date,
-   estado_civil varchar(30),
-   altura int
-) ENGINE=InnoDB;
-
-CREATE TABLE Funcionario
-(
-   id int PRIMARY KEY auto_increment,
-   cep char(10),
-   endereco varchar(100),
-   bairro varchar(50),
-   cidade varchar(50),
-   id_cliente int not null,
-   FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON DELETE CASCADE
-) ENGINE=InnoDB;
-
-CREATE TABLE Medico
-(
-   id int PRIMARY KEY auto_increment,
-   cep char(10),
-   endereco varchar(100),
-   bairro varchar(50),
-   cidade varchar(50),
-   id_cliente int not null,
-   FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON DELETE CASCADE
-) ENGINE=InnoDB;
-
-CREATE TABLE Enderecos
-(
-   id int PRIMARY KEY auto_increment,
-   nome varchar(50),
-   cpf char(14) UNIQUE,
-   email varchar(50) UNIQUE,
-   hash_senha varchar(255),
-   data_nascimento date,
-   estado_civil varchar(30),
-   altura int
+   estado varchar(10)
 ) ENGINE=InnoDB;
