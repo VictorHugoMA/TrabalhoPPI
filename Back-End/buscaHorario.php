2ae49a403135626892af91be3ad066d98a5525b5
PeSantanna <pedro.santana@ufu.br>
2022-03-11 19:38:38 -0300

    Consulta

---
+++ Back-End/buscaHorario.php
@@ -0,0 +1,36 @@
+<?php
+require "../sql/conexaoMysql.php";
+
+
+$dataAgenda = $_GET['data'] ?? '';
+$codigo = $_GET['codigo'] ?? '';
+
+$sql = <<<SQL
+    SELECT horario
+    FROM agenda_Trab
+    WHERE dataAgenda = ? and codigo_medico = ? 
+    SQL;
+
+try {
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$dataAgenda, $codigo]);
+
+
+    while ($row = $stmt->fetch()) {
+
+        array_push($horarios, $row['horario']);
+    }
+
+
+
+
+    foreach (array(8, 9, 10, 11, 12, 13, 14, 15, 16, 17) as $horario) {
+        if (!in_array($horario, $horarios))
+            array_push($disponiveis, $horario);
+    }
+
+    return json_encode($disponiveis);
+} catch (Exception $e) {
+
+    exit('Falha inesperada: ' . $e->getMessage());
+}
