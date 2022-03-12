5aa4d92757c632c9e67d594cd168070c257dcff8
PeSantanna <pedro.santana@ufu.br>
2022-03-12 14:42:09 -0300

    Internos css

--- Front-End/AcessoRestrito/formCadastroFuncionario.php
+++ Front-End/AcessoRestrito/formCadastroFuncionario.php
@@ -33,126 +33,129 @@ exitWhenNotLogged($pdo);
 
         <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active"><a class="nav-link" href="homeUsuario.php">Home Usuário</a></li>
-                <li class="nav-item"><a class="nav-link" href="formCadastroFuncionario.php">Cadastro de Funcionário</a>
+                <li class="nav-item active"><a class="nav-link color-white" href="homeUsuario.php">Home Usuário</a></li>
+                <li class="nav-item"><a class="nav-link color-white" href="formCadastroFuncionario.php">Cadastro de Funcionário</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="formCadastroPaciente.php">Cadastro de Paciente</a></li>
-                <li class="nav-item"><a class="nav-link" href="dados/mostraDadosFuncionario.php">Listar Funcionários</a>
+                <li class="nav-item"><a class="nav-link color-white" href="formCadastroPaciente.php">Cadastro de Paciente</a></li>
+                <li class="nav-item"><a class="nav-link color-white" href="dados/mostraDadosFuncionario.php">Listar Funcionários</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="dados/mostraDadosPaciente.php">Listar Pacientes</a></li>
-                <li class="nav-item"><a class="nav-link" href="dados/mostraDadosEndereco.php">Listar Endereços</a></li>
-                <li class="nav-item"><a class="nav-link" href="dados/mostraDadosConsulta.php">Listar Agendamentos</a>
+                <li class="nav-item"><a class="nav-link color-white" href="dados/mostraDadosPaciente.php">Listar Pacientes</a></li>
+                <li class="nav-item"><a class="nav-link color-white" href="dados/mostraDadosEndereco.php">Listar Endereços</a></li>
+                <li class="nav-item"><a class="nav-link color-white" href="dados/mostraDadosConsulta.php">Listar Agendamentos</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="../../Back-End/logout.php">Logout</a></li>
+                <li class="nav-item"><a class="nav-link logout-b" href="../../Back-End/logout.php">Logout</a></li>
             </ul>
         </div>
     </nav>
         <main>
-            <h3>Cadastrar novo Funcionário</h3>
-            <form action="../../Back-End/cadastraFuncionario.php" method="post">
-                    
-                    <div class="row">
-                        <div class="col-sm-8">
-                            <label for="nome" class="form-label">Nome</label>
-                            <input type="text" class="form-control" id="nome" name="nome">
-                        </div>
-                        <div class="col-sm-4">
-                            <label for="sexo" class="form-label">Sexo</label>
-                            <select name="sexo" id="sexo" class="form-select">
-                                <option value="Masculino">Masculino</option>
-                                <option value="Feminino">Feminino</option>
-                                <option value="Outro">Outro</option>
-                            </select>
-                        </div>
-                        <div class="col-sm-4">
-                            <label class="form-label" for="email">Email</label>
-                            <input class="form-control" type="email" id="email" name="email" required>
-                        </div>
-                        <div class="col-sm-3">
-                            <label class="form-label" for="telefone">Telefone</label>
-                            <input class="form-control" type="tel" id="telefone" name="telefone">
-                        </div>
+            <div class="container">
+                <h3>Cadastrar novo Funcionário</h3>
+
+                <form action="../../Back-End/cadastraFuncionario.php" method="post">
                         
-                            <div class="col-sm-5">
-                                <label for="cep" class="form-label">CEP</label>
-                                <input type="text" class="form-control" id="cep" name="cep">
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <label for="nome" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="nome" name="nome">
                             </div>
-                            <div class="col-sm-5">
-                                <label for="logradouro" class="form-label">Logradouro</label>
-                                <input type="text" class="form-control" id="logradouro" name="logradouro">
-                            </div>
-                        
-                        
                             <div class="col-sm-4">
-                                <label for="cidade" class="form-label">Cidade</label>
-                                <input type="text" class="form-control" id="cidade" name="cidade">
-                            </div>
-                            <div class="col-sm-3">
-                                <label for="estado" class="form-label">Estado</label>
-                                <select name="estado" id="estado" class="form-select">
-                                    <option value="AC">AC</option>
-                                    <option value="AL">AL</option>
-                                    <option value="AP">AP</option>
-                                    <option value="AM">AM</option>
-                                    <option value="BA">BA</option>
-                                    <option value="CE">CE</option>
-                                    <option value="ES">ES</option>
-                                    <option value="GO">GO</option>
-                                    <option value="MA">MA</option>
-                                    <option value="MT">MT</option>
-                                    <option value="MS">MS</option>
-                                    <option value="MG">MG</option>
-                                    <option value="PA">PA</option>
-                                    <option value="PB">PB</option>
-                                    <option value="PR">PR</option>
-                                    <option value="PE">PE</option>
-                                    <option value="PI">PI</option>
-                                    <option value="RJ">RJ</option>
-                                    <option value="RN">RN</option>
-                                    <option value="RS">RS</option>
-                                    <option value="RO">RO</option>
-                                    <option value="RR">RR</option>
-                                    <option value="SC">SC</option>
-                                    <option value="SP">SP</option>
-                                    <option value="SE">SE</option>
-                                    <option value="TO">TO</option>
-                                    <option value="DF">DF</option>
+                                <label for="sexo" class="form-label">Sexo</label>
+                                <select name="sexo" id="sexo" class="form-select">
+                                    <option value="Masculino">Masculino</option>
+                                    <option value="Feminino">Feminino</option>
+                                    <option value="Outro">Outro</option>
                                 </select>
                             </div>
                             <div class="col-sm-4">
-                                <label class="form-label" for="dataContrato">Data Contrato</label>
-                                <input class="form-control" type="date" id="dataContrato" name="dataContrato">
+                                <label class="form-label" for="email">Email</label>
+                                <input class="form-control" type="email" id="email" name="email" required>
                             </div>
-                            <div class="col-sm-4">
-                                <label class="form-label" for="salario">Salario</label>
-                                <input class="form-control" type="number" step=0.01 id="salario" name="salario">
-                            </div>
-                            <div class="col-sm-4">
-                                <label class="form-label" for="senha">Senha</label>
-                                <input class="form-control" type="password" id="senha" name="senha" require>
-                            </div>
-                            <h5>É médico ?</h5>
-                            <div>
-                                <input class="ehMedico .form-check-input " type="radio" onclick="verificaMedico();" id="simMedico" name="medico" value="simMedico"checked>
-                                <label for="simMedico">Sim</label>
-                                <input class="ehMedico .form-check-input " type="radio" onclick="verificaMedico();" id="naoMedico" name="medico" value="naoMedico">
-                                <label for="naoMedico">Não</label>
+                            <div class="col-sm-3">
+                                <label class="form-label" for="telefone">Telefone</label>
+                                <input class="form-control" type="tel" id="telefone" name="telefone">
                             </div>
                             
-                                <div class="col-sm-6">
-                                    <label class="form-label" for="especialidade">Especialidade</label>
-                                    <input class="form-control medico" type="text" id="especialidade" name="especialidade">
+                                <div class="col-sm-5">
+                                    <label for="cep" class="form-label">CEP</label>
+                                    <input type="text" class="form-control" id="cep" name="cep">
                                 </div>
-                                <div class="col-sm-6">
-                                    <label class="form-label" for="crm">CRM</label>
-                                    <input class="form-control medico" type="number" id="crm" name="crm">
+                                <div class="col-sm-5">
+                                    <label for="logradouro" class="form-label">Logradouro</label>
+                                    <input type="text" class="form-control" id="logradouro" name="logradouro">
                                 </div>
-                    </div>
-                    
-                    
-                    <button class="btn btn-primary my-2">Cadastrar</button>
-
-                </form>
+                            
+                            
+                                <div class="col-sm-4">
+                                    <label for="cidade" class="form-label">Cidade</label>
+                                    <input type="text" class="form-control" id="cidade" name="cidade">
+                                </div>
+                                <div class="col-sm-3">
+                                    <label for="estado" class="form-label">Estado</label>
+                                    <select name="estado" id="estado" class="form-select">
+                                        <option value="AC">AC</option>
+                                        <option value="AL">AL</option>
+                                        <option value="AP">AP</option>
+                                        <option value="AM">AM</option>
+                                        <option value="BA">BA</option>
+                                        <option value="CE">CE</option>
+                                        <option value="ES">ES</option>
+                                        <option value="GO">GO</option>
+                                        <option value="MA">MA</option>
+                                        <option value="MT">MT</option>
+                                        <option value="MS">MS</option>
+                                        <option value="MG">MG</option>
+                                        <option value="PA">PA</option>
+                                        <option value="PB">PB</option>
+                                        <option value="PR">PR</option>
+                                        <option value="PE">PE</option>
+                                        <option value="PI">PI</option>
+                                        <option value="RJ">RJ</option>
+                                        <option value="RN">RN</option>
+                                        <option value="RS">RS</option>
+                                        <option value="RO">RO</option>
+                                        <option value="RR">RR</option>
+                                        <option value="SC">SC</option>
+                                        <option value="SP">SP</option>
+                                        <option value="SE">SE</option>
+                                        <option value="TO">TO</option>
+                                        <option value="DF">DF</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm-4">
+                                    <label class="form-label" for="dataContrato">Data Contrato</label>
+                                    <input class="form-control" type="date" id="dataContrato" name="dataContrato">
+                                </div>
+                                <div class="col-sm-4">
+                                    <label class="form-label" for="salario">Salario</label>
+                                    <input class="form-control" type="number" step=0.01 id="salario" name="salario">
+                                </div>
+                                <div class="col-sm-4">
+                                    <label class="form-label" for="senha">Senha</label>
+                                    <input class="form-control" type="password" id="senha" name="senha" require>
+                                </div>
+                                <h5>É médico ?</h5>
+                                <div>
+                                    <input class="ehMedico .form-check-input " type="radio" onclick="verificaMedico();" id="simMedico" name="medico" value="simMedico"checked>
+                                    <label for="simMedico">Sim</label>
+                                    <input class="ehMedico .form-check-input " type="radio" onclick="verificaMedico();" id="naoMedico" name="medico" value="naoMedico">
+                                    <label for="naoMedico">Não</label>
+                                </div>
+                                
+                                    <div class="col-sm-6">
+                                        <label class="form-label" for="especialidade">Especialidade</label>
+                                        <input class="form-control medico" type="text" id="especialidade" name="especialidade">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <label class="form-label" for="crm">CRM</label>
+                                        <input class="form-control medico" type="number" id="crm" name="crm">
+                                    </div>
+                        </div>
+                        
+                        
+                        <button class="btn btn-primary my-2 btn-success">Cadastrar</button>
+    
+                    </form>
+            </div>
         </main>
     </div>
     <script src="../../js/ajaxEndereco.js"></script>
