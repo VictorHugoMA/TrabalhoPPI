d11f29f57bd35dea3bf1445a89f940438e16483f
VictorHugoMA <victor.alves1@ufu.br>
2022-03-12 17:47:34 -0300

    att style homeUsuario

--- Front-End/AcessoRestrito/homeUsuario.php
+++ Front-End/AcessoRestrito/homeUsuario.php
@@ -27,7 +27,7 @@ exitWhenNotLogged($pdo);
 </head>
 
 <body>
-
+    <div class="container">
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top  ">
         <a class="nav-link active col-1" href="../index.html">
             <img src="../../images/sanctum.png" alt="CLINICA SANCTUM SANCTORUM" height="80">
@@ -39,17 +39,17 @@ exitWhenNotLogged($pdo);
 
         <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active"><a class="nav-link" href="homeUsuario.php">Home Usuário</a></li>
-                <li class="nav-item"><a class="nav-link" href="formCadastroFuncionario.php">Cadastro de Funcionário</a>
+                <li class="nav-item active"><a class="nav-link color-white" href="homeUsuario.php">Home Usuário</a></li>
+                <li class="nav-item"><a class="nav-link color-white" href="formCadastroFuncionario.php">Cadastro de Funcionário</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="formCadastroPaciente.php">Cadastro de Paciente</a></li>
-                <li class="nav-item"><a class="nav-link" href="dados/mostraDadosFuncionario.php">Listar Funcionários</a>
+                <li class="nav-item"><a class="nav-link color-white" href="formCadastroPaciente.php">Cadastro de Paciente</a></li>
+                <li class="nav-item"><a class="nav-link color-white" href="dados/mostraDadosFuncionario.php">Listar Funcionários</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="dados/mostraDadosPaciente.php">Listar Pacientes</a></li>
-                <li class="nav-item"><a class="nav-link" href="dados/mostraDadosEndereco.php">Listar Endereços</a></li>
-                <li class="nav-item"><a class="nav-link" href="dados/mostraDadosConsulta.php">Listar Agendamentos</a>
+                <li class="nav-item"><a class="nav-link color-white" href="dados/mostraDadosPaciente.php">Listar Pacientes</a></li>
+                <li class="nav-item"><a class="nav-link color-white" href="dados/mostraDadosEndereco.php">Listar Endereços</a></li>
+                <li class="nav-item"><a class="nav-link color-white" href="dados/mostraDadosConsulta.php">Listar Agendamentos</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="../../Back-End/logout.php">Logout</a></li>
+                <li class="nav-item"><a class="nav-link logout-b" href="../../Back-End/logout.php">Logout</a></li>
             </ul>
         </div>
     </nav>
@@ -59,6 +59,7 @@ exitWhenNotLogged($pdo);
         <p>Bem vindo ao site, esta é sua area de usuario sit amet consectetur adipisicing elit. Quos qui debitis ab reiciendis eos, a quisquam natus quam recusandae rem voluptatem, ad fugiat. Alias voluptatibus cum fuga labore, odio autem.</p>   
         <p>Com essas opções ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iusto voluptates, expedita rerum voluptate nam harum veniam debitis odio ex quo recusandae ullam doloremque reiciendis. Cupiditate rerum sint nemo enim</p>
     </main>
+    </div>
     <!-- Bootstrap CSS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
